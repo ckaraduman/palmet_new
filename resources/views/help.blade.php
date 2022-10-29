c7dd044c610d4d18930e8930440354dac5426e22
cemilker <c.karaduman@palmet.com>
2022-10-29 18:54:30 +0300

    Bilgilerin formdan gönderilmesi için hazırlıklar tamamlandı.

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -9,31 +9,31 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </head>
 <h4>&nbsp;&nbsp;&nbsp;Yardım Talep Formu</h4>
+<form method="post">
+  @CSRF
 <h7>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kullanıcı Adı : <b>{{$name}}</b></h7><br>
 <h7>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kullanıcı E-Mail : <b>{{$email}}</b></h7><br><br>
-
-<form method="post">
   <div class="form-group"
   @CSRF
   <div class="container">
   <div class="row">
     <div class="col-3">
-      <select id="dpd1" class="form-select" aria-label="Default select example">
+      <select id="dpd1" class="form-select" aria-label="Default select example" name="select1">
         <option selected>Talep Türü</option>
-        <option value="swhlp">Yazılım Destek</option>
-        <option value="hwhlp">Donanım Destek</option>
-        <option value="swlcn">Yazılım Lisans</option>
-        <option value="newsw">Yeni Yazılım</option>
-        <option value="newhw">Yeni Donanım</option>
-        <option value="bilgi">Bilgi Talebi</option>
+        <option value="Yazılım Destek">Yazılım Destek</option>
+        <option value="Donanım Destek">Donanım Destek</option>
+        <option value="Yazılım Lisans">Yazılım Lisans</option>
+        <option value="Yeni Yazılım">Yeni Yazılım</option>
+        <option value="Yeni Donanım">Yeni Donanım</option>
+        <option value="Bilgi Talebi">Bilgi Talebi</option>
       </select>
     </div>
   </div>
   <div class="row">
     <div class="col-4">
-      
 
-      <textarea id="txt1" class="form-control" aria-label="With textarea" placeholder="Açıklamanız"></textarea>
+
+      <textarea id="txt1" class="form-control" aria-label="With textarea" placeholder="Açıklamanız" name="text1"></textarea>
       </div>
     </div>
   </div>
@@ -52,5 +52,22 @@
 
     </div>
 </form>
+<?php
+$content="";
+$select1="";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $select1 = $_POST["select1"];
+        $content = $_POST["text1"];
+        // $date2 = $_POST["date2"];
 
+
+        // $test_cem=DB::connection('mysql')->table('test')->where('id',1)
+        //                           ->value('name');
+
+        // $email = test_input($_POST["email"]);
+        // $review = test_input($_POST["review"]);
+        // $level = test_input($_POST["level"]);
+      }
+?>
+{{$name}}<br>{{$email}}<br>{{$content}}<br>{{$select1}}
 </body>
